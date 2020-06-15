ed3e3f927e3b6e2e0bc1ad48ba0145a583e1759c
h2ljh <h2ljh@naver.com>
2020-06-15 14:36:59 +0900

    Pycham Web crawl

---
+++ Pycham_workspace/study_crawl/test_soup4_01.py
@@ -0,0 +1,29 @@
+# beautifulsoup4 import
+# 외부 모듈 설치
+# beautifulsoup4
+# matplotlib
+# numpy
+# panda
+# selenium
+# requets
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=105&oid=081&aid=0003099012'
+resp = requests.get(url)
+
+if resp.status_code == 200:
+    resp.headers
+    
+else:
+    print('잘못된 URL입니다. 다시 입력해주세요')
+
+soup = BeautifulSoup(resp.text, 'html.parser')
+title = soup.find('h3', id='articleTitle')
+body = soup.find('div', id='articleBodyContents')
+
+print(title.text)
+print('=====================')
+print(body.text.strip()) # strip 좌우 여백없애는 함수
+
